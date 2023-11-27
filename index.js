5b9f479d79279624a3e1254a774d200b389cf415
Aditya Bhatt <adityabhatt789@gmail.com>
2023-11-27 13:32:04 +0530

    Function starting

--- index.js
+++ index.js
@@ -11,4 +11,21 @@
 // console.log(typeof(a))
 
 // 
+// const arr = [1,2]
+// const [a,b] = [[1,2],3];
+// console.log(a,b)
 
+// const person ={
+//   name:"Adi",
+// }
+// const key  = "email"
+// person['key'] = 'aditya';
+
+// console.log(person)
+
+//! Functions Begins
+
+
+function sing(){
+  
+}
