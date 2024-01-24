81adb8fd0c9b25c861c6b7bbbb04a40f9041a3b2
Aditya Bhatt <adityabhatt789@gmail.com>
2024-01-24 14:10:23 +0530

    tut 001

---
+++ 001/01.js
@@ -0,0 +1,33 @@
+// console.log("hello World")
+// this can print anything on console
+// this is a shortcut for wiring the comments in Javascript
+
+
+// !  VARIABLES
+
+//  var firstName = "Aditya"
+//  console.log(firstName)
+
+   
+// string indexing
+
+// string.length // gives length of string
+// string[1] // get index of desired alphabet
+
+// let firstName = "Aditya"
+// console.log(firstName.length)
+// console.log(firstName[firstName.length-1 ])
+
+// let firstName;
+// console.log(firstName)
+ 
+// let num=10;
+// let sum = 0;
+// while(num>0){
+//   sum+=num;
+//   num--;
+// }
+
+// console.log(sum)
+
+
--- index.js
+++ index.js
@@ -140,3 +140,177 @@
 // }
 // console.log(mul(2)(3)(4)); // output : 24
 
+
+
+//! empty an array 
+
+// var arrayList =  ['a', 'b', 'c', 'd', 'e', 'f'];
+// let res = (arrayList.splice(0,arrayList.length))
+// console.log(arrayList)
+// console.log(res)
+
+// console.log(undefined*1)
+
+
+// var bar = true;
+// console.log(bar + 0);   
+// console.log(bar + "xyz");  
+// console.log(bar + true);  
+// console.log(bar + false);
+
+
+
+// var z = 1, y = z = typeof y;
+// console.log(y);
+
+
+
+// var z;
+// z = 1;
+// var y;
+// y = z;
+// z = typeof y;
+// console.log(y)
+
+// bar()
+// var bar= function() { console.log(11); };
+
+
+// var salary = "1000$";
+
+// (function () {
+//   var salary = undefined;
+//   console.log("Original salary was " + salary);
+
+//   salary = "5000$";
+
+//   console.log("My New Salary " + salary);
+// })();
+
+
+
+// function get(){
+//   console.log(a)
+//   const a = 10
+// }
+
+// get()
+
+
+
+/**
+ * & HOOKS
+ * 
+  useState
+  useEffect
+  Use Context
+  Use Reducer
+  Use Memo
+  Use Callbacks
+  Use Ref
+ * 
+ * 
+ * & HIGHER ORDER COMPONENTS
+ * 
+ * what?
+ * why?
+ * when to use?
+ * how?
+ * 
+ * 
+ * 
+ * & LIFE CYCLE METHODS OF COMPONENTS
+ * 
+ * 3 phases
+ * mounting 
+ * updating
+ * deleting
+ * 
+ * 
+ * & State Management
+ * 
+ * States/props 
+ * Props drilling
+ * Context
+ * 
+ * 
+ * & REDUX/REDUX TOOLKIT
+ * 
+ * how
+ * why
+ * when
+ * 
+ * 
+ * & CUSTOMS HOOKS (GOOD COMPANY)
+ * 
+ * when 
+ * how 
+ * code
+ * 
+ * 
+ * & LAZY LOADING/CHUNKING (advance)
+ * 
+ * & VIRTUAL DOM
+ * 
+ * reconciliation
+ * react fiber
+ * 
+ * 
+ * &SERVER SIDE RENDERING vs CLIENT SIDE RENDERING 🔥
+ * 
+ * seo and Performance
+ * 
+ * 
+ * & ROUTING
+ * -react router 
+ * - protected router
+ * - role based access control
+ * - qwery params
+ * - Dynamic Routing
+ * 
+ * & Testing
+ * 
+ * 
+ * & Async Task
+ * - UseEffect in depth(fetch,Axios)
+ * - Promises
+ * - api calls
+ * - Events
+ * 
+ * 
+ * & REUSABILITY
+ * & MODULARITY
+ * & TESTABILITY
+ * & PERFORMANCE
+ * 
+ * & TAILWIND
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// const Github_Api = "https://www.abibliadigital.com.br/api/verses/nvi/sl/23"
+
+// const user = fetch(Github_Api);
+// console.log(user)
+
+// console.log("Adi")
+
+let arrOne = [32, 45, 63, 36, 24, 11]
+const multFive = (num) => {
+return num * 5; //'num' here, is the value of the array.
+}
+let arrTwo = arrOne.map(multFive)
+console.log(arrTwo)
+console.log(arrOne)
+
+// let arrNum = [15, 39, 20, 32, 30, 45, 22]
+// function divByFive(num) {
+//   return num % 3 == 0
+// }
+// let arrNewNum = arrNum.filter(divByFive)
+// console.log(arrNum)
+// console.log(arrNewNum)
